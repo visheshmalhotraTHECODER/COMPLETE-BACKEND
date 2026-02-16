cdca8a70a4e1631320d80df386a0769572b84364
visheshmalhotraTHECODER <visheshmalhotra2023@gmail.com>
2026-02-16 16:35:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+
+console.log(3+3);
+
+console.log("vishesh")
+
+let name = "Vishesh"
+let age = 21
+let isloggedIn = true
+let state;
+
+// number
+// BigInt
+// string
+
+console.log(typeof null)
